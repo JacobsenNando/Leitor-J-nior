a3553b30a01da5cf5bbe91f3af878dbbdb02b046
Lucas Brizola <103971981+Lucas-Brizola-Git@users.noreply.github.com>
2024-04-10 21:03:37 +0000

    Desenvolvimento da função de cadastro, adicionado Usuarios a area adm, realizado migrations tabela usuario

--- Projeto_Leitor_Junior/usuarios/views.py
+++ Projeto_Leitor_Junior/usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Usuario
+from django.shortcuts import redirect
 
 def login(request):
     return HttpResponse('login')
@@ -8,8 +9,45 @@ def login(request):
 def cadastro(request):
     return render(request, 'cadastro.html')
 
+
+#--------------------Cadastro--------------------------
+
 def valida_cadastro(request):
     nome = request.POST.get('nome')
     email = request.POST.get('email')
     senha = request.POST.get('senha')
-    return HttpResponse(f"{nome}{email}{senha}")
+
+    
+
+#---Verificações
+####################--Status--#######################
+"""                                                 #
+0 = Cadastro realizado com sucesso                  #
+1 = Falha no cadastro, nome ou email em branco      #
+2 = Falha no cadastro, senha menor que 8 caracteres #
+3 = Falha no cadastro, email já cadastrado          #
+4 = Erro ao realizar cadastro                       #
+                                                    #
+"""                                                 #
+#####################################################
+    
+    usuario = Usuario.objects.filter(email = email)
+    
+    if len(nome.strip())  == 0 or len(email.strip)) == 0;
+        return redirect('/auth/cadastro/?status=1')
+
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=2')
+
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=3')
+    
+    try:
+        usuario = Usuario(nome = nome, 
+                          senha = senha, 
+                        email = email)
+        usuario.save()
+
+        return redirect('/auth/login/?status=0')
+    except:
+        return redirect('/auth/cadastro/?status=4')
