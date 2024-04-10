22c22353bb0232b2b2fbac07b62c8eb41c41b767
Lucas Brizola <lukas.bryzzola@hotmail.com>
2024-04-09 22:18:36 -0300

    Criada classe usuario em models e testado cadastro no front

--- Projeto_Leitor_Junior/leitor_junior/settings.py
+++ Projeto_Leitor_Junior/leitor_junior/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "leitor_junior.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'ptemplates')],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
