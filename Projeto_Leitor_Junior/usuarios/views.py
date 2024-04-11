30d213aa0518a3b8075b4285a583e26b98a33ef1
Lucas Brizola <lukas.bryzzola@hotmail.com>
2024-04-10 22:27:00 -0300

    Ajustado comentarios usuarios.views

--- Projeto_Leitor_Junior/usuarios/views.py
+++ Projeto_Leitor_Junior/usuarios/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Usuario
 from django.shortcuts import redirect
+from hashlib import sha256
+
+"""
+#---Verificações
+####################--Status--#######################
+                                                #
+0 = Cadastro realizado com sucesso                  #
+1 = Falha no cadastro, nome ou email em branco      #
+2 = Falha no cadastro, senha menor que 8 caracteres #
+3 = Falha no cadastro, email já cadastrado          #
+4 = Erro ao realizar cadastro                       #
+                                                    #
+                                                 #
+#####################################################
+   """ 
 
 def login(request):
     return HttpResponse('login')
@@ -17,23 +32,11 @@ def valida_cadastro(request):
     email = request.POST.get('email')
     senha = request.POST.get('senha')
 
-    
+  
 
-#---Verificações
-####################--Status--#######################
-"""                                                 #
-0 = Cadastro realizado com sucesso                  #
-1 = Falha no cadastro, nome ou email em branco      #
-2 = Falha no cadastro, senha menor que 8 caracteres #
-3 = Falha no cadastro, email já cadastrado          #
-4 = Erro ao realizar cadastro                       #
-                                                    #
-"""                                                 #
-#####################################################
-    
     usuario = Usuario.objects.filter(email = email)
     
-    if len(nome.strip())  == 0 or len(email.strip)) == 0;
+    if len(nome.strip())  == 0 or len(email.strip()) == 0:
         return redirect('/auth/cadastro/?status=1')
 
     if len(senha) < 8:
@@ -43,6 +46,7 @@ def valida_cadastro(request):
         return redirect('/auth/cadastro/?status=3')
     
     try:
+        #senha = sha256(senha.encode)
         usuario = Usuario(nome = nome, 
                           senha = senha, 
                         email = email)
