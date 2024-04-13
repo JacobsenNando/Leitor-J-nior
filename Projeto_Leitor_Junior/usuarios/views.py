6c11fac821a0c277eea656268394bda990a274c3
Lucas Brizola <103971981+Lucas-Brizola-Git@users.noreply.github.com>
2024-04-13 07:38:33 +0000

    Finalizada funcionalidades de login e logout, realizado roteamento, para login, logout e valida_login. Realizado roteamento para homepage e realizada validação para usuario logado

--- Projeto_Leitor_Junior/usuarios/views.py
+++ Projeto_Leitor_Junior/usuarios/views.py
@@ -4,20 +4,6 @@ from .models import Usuario
 from django.shortcuts import redirect
 from hashlib import sha256
 
-"""
-#---Verificações
-####################--Status--#######################
-                                                #
-0 = Cadastro realizado com sucesso                  #
-1 = Falha no cadastro, nome ou email em branco      #
-2 = Falha no cadastro, senha menor que 8 caracteres #
-3 = Falha no cadastro, email já cadastrado          #
-4 = Erro ao realizar cadastro                       #
-                                                    #
-                                                 #
-#####################################################
-   """ 
-
 def login(request):
     return render(request, 'login.html')
 
@@ -26,6 +12,18 @@ def cadastro(request):
 
 
 #--------------------Cadastro--------------------------
+
+"""
+################--Status Cadastro--##################
+                                                    #
+0 = Cadastro realizado com sucesso                  #
+1 = Falha no cadastro, nome ou email em branco      #
+2 = Falha no cadastro, senha menor que 8 caracteres #
+3 = Falha no cadastro, email já cadastrado          #
+4 = Erro ao realizar cadastro                       #
+                                                    #
+#####################################################
+""" 
 
 def valida_cadastro(request):
     nome = request.POST.get('nome')
@@ -36,6 +34,7 @@ def valida_cadastro(request):
 
     usuario = Usuario.objects.filter(email = email)
     
+    #---Verificações
     if len(nome.strip())  == 0 or len(email.strip()) == 0:
         return redirect('/auth/cadastro/?status=1')
 
@@ -45,6 +44,7 @@ def valida_cadastro(request):
     if len(usuario) > 0:
         return redirect('/auth/cadastro/?status=3')
     
+    #---Salva no BD
     try:
         senha = sha256(senha.encode()).hexdigest()
         usuario = Usuario(nome = nome, 
@@ -56,7 +56,19 @@ def valida_cadastro(request):
     except:
         return redirect('/auth/cadastro/?status=4')
     
-#--------------------Login--------------------#
+#--------------------Login-----------------------------
+
+"""
+#################--Status Login--####################
+                                                    #
+0 = Cadastro realizado com sucesso                  #
+1 = Falha no login, email não cadastrado            #
+2 = Falha ao acessar pafina, usuário não logado     #
+3 =                                                 #
+4 =                                                 #
+                                                    #
+#####################################################
+""" 
 
 def valida_login(request):
     email = request.POST.get('email')
@@ -64,26 +76,19 @@ def valida_login(request):
 
     senha = sha256(senha.encode()).hexdigest()
 
+    #Busca usuário que coincide com email e senha
     usuario = Usuario.objects.filter(email = email).filter(senha = senha)
-
-    #return HttpResponse(f"{email} {senha}")
     
+    #Verifica se há usuario conforme email e senha passado, se sim, cria uma session com o id do usuario
     if len(usuario) == 0:
         return redirect('/auth/login/?status=1')
+    elif len(usuario) > 1:
+        request.session['usuario'] = usuario[0].id
+        return redirect(f'/livro/home/?id_usuario={request.session["usuario"]}')
 
-    if len(senha) < 8:
-        return redirect('/auth/login/?status=2')
 
-    if len(usuario) > 0:
-        return redirect('/auth/login/?status=3')
-    
-    try:
-        senha = sha256(senha.encode()).hexdigest()
-        usuario = Usuario(nome = nome, 
-                          senha = senha, 
-                          email = email)
-        usuario.save()
-
-        return redirect('/auth/login/?status=0')
-    except:
-        return redirect('/auth/cadastro/?status=4')
+#---------------------Logout--------------------------
+#Limpa session para fazer logout do usuário do sistema
+def logout(request):
+    request.session.flush()
+    return redirect('/auth/login/')
