4747a66490faa9f1babbe0d83051cb895f5e777a
Lucas Brizola <lukas.bryzzola@hotmail.com>
2024-04-17 19:26:49 -0300

    iniciado busca e filtro, adicionado url, ajustado homepage

--- livro/views.py
+++ livro/views.py
@@ -8,3 +8,21 @@ def  home(request):
         #return HttpResponse('Homepage')
     else:
         return redirect('/auth/login/?status=2')
+
+def search(request):
+    if request.session.get("usuario"):
+        busca = request.POST.get('search')
+        filtro = int(request.POST.get('filtro'))
+        #return HttpResponse(f'{filtro}')
+        if filtro == 1:
+            return HttpResponse('1')
+        elif filtro == 2:
+            return HttpResponse('2')
+        elif filtro == 3:
+            return HttpResponse('3')
+        else:
+            return HttpResponse('fail')
+
+
+        resultado = Livros.objects.filter()
+        return render(request, 'homepage.html')
