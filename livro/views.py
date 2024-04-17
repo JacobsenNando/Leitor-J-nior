a47effc910d4e023681f1b6d8ee288a2565681e0
¨JacobsenNando¨ <fernandofjlr@gmail.com>
2024-04-17 12:03:20 -0300

    Update homepage

--- livro/views.py
+++ livro/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-def  home(request):
-    if request.session.get('usuario'):
-        return render(request, 'homepage.html')
-        #return HttpResponse('Homepage')
+
+def home(request):
+    if request.session.get("usuario"):
+        return render(request, "homepage.html")
+        # return HttpResponse('Homepage')
     else:
-        return redirect('/auth/login/?status=2')
+        return redirect("/auth/login/?status=2")
