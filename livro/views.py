e514f47ca206aa65eaf27541959412a03d4c6e17
Lucas Brizola <lukas.bryzzola@hotmail.com>
2024-04-20 21:08:31 -0300

    Adicionado metodo para remover espaçoes em branco do valor de busca

--- livro/views.py
+++ livro/views.py
@@ -16,21 +16,18 @@ def search(request):
     if request.session.get("usuario"):
         busca = request.POST.get('search')
         filtro = request.POST.get('filtro')
-        padrao = r"^[a-zA-Z0-9][a-zA-Z0-9\s]*[a-zA-Z0-9]$"
-        if re.match(padrao, busca):
-            return HttpResponse('ok')
-        else:
-            return HttpResponse('falha')
-        
-        if (filtro != 'titulo') and (filtro !='autor') and (filtro !='genero') :#Verifica se não foi adicionado um valor arbitrário pelo usuario em filtros
-            return HttpResponseForbidden() #Se o valor for difrente dos permitidos retorna 403
-        
-                                                #usando **kwargss
-        resultado = Livros.objects.filter(**{f'{filtro}__icontains':busca}) #Busca no banco de dados, no atributo passado por "filtro" 
-                                                                            #todos os livros que cujo os quais contenham o valor passado em "busca"
-                                                                            #sem difrenciar maiúsculas e minusculas(__icontains)
-        if len(resultado) == 0:
-            return HttpResponse('Nenhum resultado correspondente')
-        elif len(resultado) > 0:
-            #return HttpResponse(f'{resultado[0].titulo}')
-            return render(request, 'homepage.html', {'livros' : resultado})
+        re_busca = r"^[a-zA-Z0-9][a-zA-Z0-9\s]*[a-zA-Z0-9]$" #Regex para validar que que a entrada do usuário é somente uma string alfanumerica sem caracteres especiais
+        if re.match(re_busca, busca.strip()):
+
+            if (filtro != 'titulo') and (filtro !='autor') and (filtro !='genero') :#Verifica se não foi adicionado um valor arbitrário pelo usuario em filtros
+                return HttpResponseForbidden() #Se o valor for difrente dos permitidos retorna 403
+            
+                                                    #usando **kwargss
+            resultado = Livros.objects.filter(**{f'{filtro}__icontains':busca}) #Busca no banco de dados, no atributo passado por "filtro" 
+                                                                                #todos os livros que cujo os quais contenham o valor passado em "busca"
+                                                                                #sem difrenciar maiúsculas e minusculas(__icontains)
+            if len(resultado) == 0:
+                return HttpResponse('Nenhum resultado encontrado')
+            elif len(resultado) > 0:
+                #return HttpResponse(f'{resultado[0].titulo}')
+                return render(request, 'homepage.html', {'livros' : resultado})
