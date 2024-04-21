71c5d07f60f8eaea7a8357646a614457eb596359
Lucas Brizola <lukas.bryzzola@hotmail.com>
2024-04-21 12:13:37 -0300

    Adicionado path em urls para direcionar localhost:8000 para homepage

--- livro/urls.py
+++ livro/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name = 'home'),
     path("home/", views.home, name = 'home'),
     path("search/", views.search, name = 'search'),
     path("ver_livro/<int:id>", views.ver_livro, name = 'ver_livro') 
