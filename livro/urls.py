315cdd39153f3fcb5179b679983f66f8956a1e73
JacobsenNando <fernandofjlr@gmail.com>
2024-04-21 21:38:11 -0300

    tentativa de acerto

--- livro/urls.py
+++ livro/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name = 'home'),
-    path("home/", views.home, name = 'home'),
-    path("search/", views.search, name = 'search'),
-    path("ver_livro/<int:id>", views.ver_livro, name = 'ver_livro') 
+    path("", views.home, name='home'),
+    path("home/", views.home, name='home'),
+    path("search/", views.search, name='search'),
+    path("ver_livro/<int:id>", views.ver_livro, name='ver_livro'),
+    path("search/<int:page>", views.search, name='search_paginated'),  # Adiciona a URL para paginação
 ]
